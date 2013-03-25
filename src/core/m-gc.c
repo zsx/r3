b1ed1b07ffed106b22391e73b61a5310416f0e8b
ladislav <ladislav@0b15d6fc-ce89-4483-8625-0b0fabc32e25>
2013-03-25 17:02:39 +0000

    Collect unused GOBs Resolving bug#1989. Define new GOB_MARK and GOB_USED GC flags. Use the resv field to store GC flags.
    git-svn-id: https://s1.saphirion.com/svn/rm-asset/trunk/r3@4820 0b15d6fc-ce89-4483-8625-0b0fabc32e25

--- src/core/m-gc.c
+++ src/core/m-gc.c
@@ -115,26 +115,29 @@ static void Mark_Series(REBSER *series, REBCNT depth);
 	REBGOB **pane;
 	REBCNT i;
 
-	if (GOB_PANE(gob) && !IS_MARK_SERIES(GOB_PANE(gob))) {
-		MARK_SERIES(GOB_PANE(gob));
-		pane = GOB_HEAD(gob);
-		for (i = 0; i < GOB_TAIL(gob); i++, pane++) {
-			Mark_Gob(*pane, depth);
+	if (!IS_GOB_MARK(gob)) {
+		MARK_GOB(gob);
+		if (GOB_PANE(gob)) {
+			MARK_SERIES(GOB_PANE(gob));
+			pane = GOB_HEAD(gob);
+			for (i = 0; i < GOB_TAIL(gob); i++, pane++) {
+				Mark_Gob(*pane, depth);
+			}
 		}
-	}
-
-	if (GOB_PARENT(gob)) Mark_Gob(GOB_PARENT(gob), depth);
-
-	if (GOB_CONTENT(gob)) {
-		if (GOB_TYPE(gob) >= GOBT_IMAGE && GOB_TYPE(gob) <= GOBT_STRING) {
-			MARK_SERIES(GOB_CONTENT(gob));
-		} else if (GOB_TYPE(gob) >= GOBT_DRAW && GOB_TYPE(gob) <= GOBT_EFFECT) {
-			CHECK_MARK(GOB_CONTENT(gob), depth);
+	
+		if (GOB_PARENT(gob)) Mark_Gob(GOB_PARENT(gob), depth);
+	
+		if (GOB_CONTENT(gob)) {
+			if (GOB_TYPE(gob) >= GOBT_IMAGE && GOB_TYPE(gob) <= GOBT_STRING) {
+				MARK_SERIES(GOB_CONTENT(gob));
+			} else if (GOB_TYPE(gob) >= GOBT_DRAW && GOB_TYPE(gob) <= GOBT_EFFECT) {
+				CHECK_MARK(GOB_CONTENT(gob), depth);
+			}
 		}
-	}
-
-	if (GOB_DATA(gob) && GOB_DTYPE(gob) && GOB_DTYPE(gob) != GOBD_INTEGER) {
-		CHECK_MARK(GOB_DATA(gob), depth);
+	
+		if (GOB_DATA(gob) && GOB_DTYPE(gob) && GOB_DTYPE(gob) != GOBD_INTEGER) {
+			CHECK_MARK(GOB_DATA(gob), depth);
+		}
 	}
 }
 
@@ -409,6 +412,41 @@ mark_obj:
 
 /***********************************************************************
 **
+*/	static REBCNT Sweep_Gobs(void)
+/*
+**		Free all unmarked gobs.
+**
+**		Scans all gobs in all segments that are part of the
+**		GOB_POOL. Free gobs that have not been marked.
+**
+***********************************************************************/
+{
+	REBSEG	*seg;
+	REBGOB	*gob;
+	REBCNT  n;
+	REBCNT	count = 0;
+
+	for (seg = Mem_Pools[GOB_POOL].segs; seg; seg = seg->next) {
+		gob = (REBGOB *) (seg + 1);
+		for (n = Mem_Pools[GOB_POOL].units; n > 0; n--) {
+			if (IS_GOB_USED(gob)) {
+				if (IS_GOB_MARK(gob))
+					UNMARK_GOB(gob);
+				else {
+					Free_Gob(gob);
+					count++;
+				}
+			}
+			gob++;
+		}
+	}
+
+	return count;
+}
+
+
+/***********************************************************************
+**
 */	REBCNT Recycle(void)
 /*
 **		Recycle memory no longer needed.
@@ -475,6 +513,7 @@ mark_obj:
 	Mark_Series(Task_Series, 0);
 
 	count = Sweep_Series();
+	count += Sweep_Gobs();
 
 	CHECK_MEMORY(4);
 
