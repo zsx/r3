a3632976d70c29253111b5f2a94f879610aea84c
shixinzeng <shixinzeng@0b15d6fc-ce89-4483-8625-0b0fabc32e25>
2013-10-16 21:06:31 +0000

    Do not depend on xlibrgb
    Use AGG for pixmap format conversion when neccessary. The conversion is
    not really tested.
    
    git-svn-id: https://s1.saphirion.com/svn/rm-asset/trunk/r3@5389 0b15d6fc-ce89-4483-8625-0b0fabc32e25

--- src/os/linux/host-window.h
+++ src/os/linux/host-window.h
@@ -4,14 +4,47 @@
 #include  <X11/Xlib.h>
 #include  <X11/Xatom.h>
 #include  <X11/Xutil.h>
- 
-typedef struct {
-	Window 	x_window;
-	GC		x_gc;
-	char	*pixbuf;
-	REBCNT	pixbuf_len;
-} host_window_t;
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+	typedef enum {
+		pix_format_undefined,
+		pix_format_bgr555 = 0,
+		pix_format_bgr565,
+		pix_format_rgba32,
+		pix_format_bgra32
+	} pixmap_format_t;
 
-void* Find_Window(REBGOB *gob);
+	typedef struct {
+		Display *display;
+		Screen 	*default_screen;
+		Visual 	*default_visual;
+		int 	default_depth;
+		int		bpp;
+		pixmap_format_t sys_pixmap_format;
+	} x_info_t;
+
+	typedef struct {
+		Window 			x_window;
+		GC				x_gc;
+		XImage			*x_image;
+		pixmap_format_t pixmap_format;
+		REBYTE			*pixbuf;
+		REBCNT			pixbuf_len;
+	} host_window_t;
+
+	extern x_info_t *global_x_info;
+
+	void* Find_Window(REBGOB *gob);
+	REBGOB *Find_Gob_By_Window(Window win);
+	void OS_Init_Windows();
+	void OS_Update_Window(REBGOB *gob);
+	void* OS_Open_Window(REBGOB *gob);
+	void OS_Close_Window(REBGOB *gob);
+
+#ifdef __cplusplus
+}
+#endif
+
 #endif
