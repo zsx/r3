e9dcea1d2718d90addb51c3333afe4b3a75a03b8
Ladislav <lmecir@volny.cz>
2014-02-18 19:10:43 +0100

    Suppress compiler warnings. Delete REBDCL type definition (no longer used in code). Based on mul64 since it updates reb-c.h Get rid of INT_64_MODE (the code is not consistent with its opposite anyway) Use C99 integer definitions where available Use typedef long i32 for non-C99 and non LP64 compilers
    Conflicts:
    	src/core/f-math.c
    	src/include/reb-c.h

--- src/core/f-math.c
+++ src/core/f-math.c
@@ -210,6 +210,7 @@
 	return buf + len;
 }
 
+
 /***********************************************************************
 **
 */	REBYTE *Form_Integer(REBYTE *buf, REBI64 val)
@@ -242,7 +243,7 @@
 
 REBINT Emit_Decimal(REBYTE *cp, REBDEC d, REBFLG trim, REBYTE point, REBINT decimal_digits) {
 	REBYTE *start = cp, *sig, *rve;
-	REBINT e, sgn;
+	int e, sgn;
 	REBINT digits_obtained;
 
 	/* sanity checks */
