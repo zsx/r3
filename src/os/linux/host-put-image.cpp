bc36ac25f6b4a4dd69e3bf59dfdeced597822e7d
Shixin Zeng <szeng@atronixengineering.com>
2014-03-03 11:27:32 -0500

    [Linux] Fix put_image
    image->data has to restored
    
    width_stride has to be corrected
    
    This is tested on Raspberry Pi with bpp = 16

--- src/os/linux/host-put-image.cpp
+++ src/os/linux/host-put-image.cpp
@@ -1,11 +1,15 @@
 #include  <X11/Xlib.h>
 #include  <X11/Xutil.h>
+#include  <assert.h>
 
-#include "reb-host.h"
-#include "host-lib.h" //for OS_Make
-#include "host-window.h"
+extern "C" {
+	#include "reb-host.h"
+	#include "host-lib.h" //for OS_Make
+	#include "host-window.h"
+}
 #include "util/agg_color_conv.h"
 #include "util/agg_color_conv_rgb8.h"
+
 
 extern "C" void put_image(Display *display,
 						  Drawable drawable,
@@ -15,10 +19,12 @@ extern "C" void put_image(Display *display,
 						  int h,
 						  pixmap_format_t sys_pixmap_format)
 {
+		char *old_data = image->data;
 		agg::rendering_buffer src((unsigned char*)image->data, w, h, w * 4);
-		unsigned char *tmp = new unsigned char [4 * w * h];
-		agg::rendering_buffer dest(tmp, w, h, w * 4);
-		switch(sys_pixmap_format){
+		unsigned char *tmp = new unsigned char [global_x_info->bpp / 8 * w * h];
+		assert(tmp != (unsigned char*) image->data);
+		agg::rendering_buffer dest(tmp, w, h, w * global_x_info->bpp / 8);
+		switch (sys_pixmap_format) {
 			case pix_format_bgr555:
 				agg::color_conv(&dest, &src, agg::color_conv_bgra32_to_rgb555());
 				break;
@@ -29,15 +35,18 @@ extern "C" void put_image(Display *display,
 				agg::color_conv(&dest, &src, agg::color_conv_bgra32_to_rgba32());
 				break;
 			default:
-				break;
+				RL_Print((REBYTE*)"Unsupported pix_format: %d\n", sys_pixmap_format);
+				delete [] tmp;
+				return;
 		}
 		image->data = (char*)tmp;
-		XPutImage (display,
-				   drawable,
-				   gc,
-				   image,
-				   0, 0,	//src x, y
-				   0, 0,	//dest x, y
-				   w, h);
+		XPutImage(display,
+				drawable,
+				gc,
+				image,
+				0, 0,	//src x, y
+				0, 0,	//dest x, y
+				w, h);
 		delete [] tmp;
+		image->data = old_data;
 }
