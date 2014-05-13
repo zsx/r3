rebol/rebol

Reduce mem/time for boot and tighten up sizes (appears to fix MSVC build)

src/core/b-init.c
@@ -148,34 +148,33 @@ extern const REBYTE Str_Banner[];
 ***********************************************************************/
 {
 	REBSER *boot;
-	REBSER *text;
-	REBSER spec;
-	REBYTE *cp;
-	REBINT i;
-	// checksum/secure "REBOL Version 3.0"
-	REBYTE key[20] = {158, 81, 15, 243, 51, 26, 109, 106, 44, 225, 75, 73, 160, 43, 1, 208, 225, 218, 150, 237};
-	REBYTE *code;
 
-	code = Make_Mem(NAT_SPEC_SIZE+1);
-	memcpy(code, Native_Specs, NAT_SPEC_SIZE); // because decloak will modify it
+	// Decompress binary data in Native_Specs to get the textual source
+	// of the function specs of the native routines.  (This compressed
+	// array lives in b-boot.c which is generated by make-boot.r)
+	// Then load that into a Rebol series as `boot`.  Note that the
+	// first four bytes of Native_Specs is a little-endian 32-bit
+	// length of the uncompressed spec data.
+	{
+		REBSER spec;
+		REBSER *text;
+		REBINT textlen;
 
-#ifdef NON_ENCODED_BOOT
-	boot = Scan_Source(code, NAT_SPEC_SIZE);
-	text = 0;
-#else
-	Cloak(TRUE, code, NAT_SPEC_SIZE, &key[0], 20, TRUE);
-	spec.data = code;
-	spec.tail = NAT_SPEC_SIZE;
-	text = Decompress(&spec, 0, -1, 200000, 0);
-	Free_Mem(code, NAT_SPEC_SIZE+1);
-	if (!text) Crash(RP_BOOT_DATA);
-	//Print("Boot Text Len: %d", STR_LEN(text));
-	boot = Scan_Source(STR_HEAD(text), STR_LEN(text));
-#endif
+		// REVIEW: This is a nasty casting away of a const.  But there's
+		// nothing that can be done about it as long as Decompress takes
+		// a REBSER, as the data field is not const
+		spec.data = ((REBYTE*)Native_Specs) + 4;
+		spec.tail = NAT_SPEC_SIZE;
 
-	//Dump_Block_Raw(boot, 0, 2);
+		textlen = Bytes_To_Long(Native_Specs);
+		text = Decompress(&spec, 0, -1, textlen, 0);
+		if (!text || (STR_LEN(text) != textlen)) Crash(RP_BOOT_DATA);
+		boot = Scan_Source(STR_HEAD(text), textlen);
+		//Dump_Block_Raw(boot, 0, 2);
+		Free_Series(text);
+	}
+
 	Set_Root_Series(ROOT_BOOT, boot, "boot block");	// Do not let it get GC'd
-	if (text) Free_Series(text);
 
 	Boot_Block = (BOOT_BLK *)VAL_BLK(BLK_HEAD(boot));
 
@@ -183,15 +182,18 @@ extern const REBYTE Str_Banner[];
 	ASSERT(VAL_WORD_SYM(VAL_BLK(&Boot_Block->types)) == SYM_END_TYPE, RP_BAD_END_TYPE_WORD);
 
 	// Create low-level string pointers (used by RS_ constants):
-	PG_Boot_Strs = (REBYTE **)Make_Mem(RS_MAX * sizeof(REBYTE *));
-	*ROOT_STRINGS = Boot_Block->strings;
-	cp = VAL_BIN(ROOT_STRINGS);
-	for (i = 0; i < RS_MAX; i++) {
-		BOOT_STR(i,0) = cp;
-		while (*cp++);
-	}
+	{
+		REBYTE *cp;
+		REBINT i;
 
-//	*ROOT_SCRIPT = Boot_Block->script;
+		PG_Boot_Strs = (REBYTE **)Make_Mem(RS_MAX * sizeof(REBYTE *));
+		*ROOT_STRINGS = Boot_Block->strings;
+		cp = VAL_BIN(ROOT_STRINGS);
+		for (i = 0; i < RS_MAX; i++) {
+			BOOT_STR(i,0) = cp;
+			while (*cp++);
+		}
+	}
 
 	ASSERT(!CMP_BYTES("end!", Get_Sym_Name(SYM_END_TYPE)), RP_BAD_END_CANON_WORD);
 	ASSERT(!CMP_BYTES("true", Get_Sym_Name(SYM_TRUE)), RP_BAD_TRUE_CANON_WORD);

zsx/r3

Conflicts:
	src/core/f-stubs.c

src/core/b-init.c
@@ -153,34 +153,33 @@ extern const REBYTE Str_Banner[];
 ***********************************************************************/
 {
 	REBSER *boot;
-	REBSER *text;
-	REBSER spec;
-	REBYTE *cp;
-	REBINT i;
-	// checksum/secure "REBOL Version 3.0"
-	REBYTE key[20] = {158, 81, 15, 243, 51, 26, 109, 106, 44, 225, 75, 73, 160, 43, 1, 208, 225, 218, 150, 237};
-	REBYTE *code;
 
-	code = Make_Mem(NAT_SPEC_SIZE+1);
-	memcpy(code, Native_Specs, NAT_SPEC_SIZE); // because decloak will modify it
+	// Decompress binary data in Native_Specs to get the textual source
+	// of the function specs of the native routines.  (This compressed
+	// array lives in b-boot.c which is generated by make-boot.r)
+	// Then load that into a Rebol series as `boot`.  Note that the
+	// first four bytes of Native_Specs is a little-endian 32-bit
+	// length of the uncompressed spec data.
+	{
+		REBSER spec;
+		REBSER *text;
+		REBINT textlen;
 
-#ifdef NON_ENCODED_BOOT
-	boot = Scan_Source(code, NAT_SPEC_SIZE);
-	text = 0;
-#else
-	Cloak(TRUE, code, NAT_SPEC_SIZE, &key[0], 20, TRUE);
-	spec.data = code;
-	spec.tail = NAT_SPEC_SIZE;
-	text = Decompress(&spec, 0, -1, 200000, 0);
-	Free_Mem(code, NAT_SPEC_SIZE+1);
-	if (!text) Crash(RP_BOOT_DATA);
-	//Print("Boot Text Len: %d", STR_LEN(text));
-	boot = Scan_Source(STR_HEAD(text), STR_LEN(text));
-#endif
+		// REVIEW: This is a nasty casting away of a const.  But there's
+		// nothing that can be done about it as long as Decompress takes
+		// a REBSER, as the data field is not const
+		spec.data = ((REBYTE*)Native_Specs) + 4;
+		spec.tail = NAT_SPEC_SIZE;
 
-	//Dump_Block_Raw(boot, 0, 2);
+		textlen = Bytes_To_REBCNT(Native_Specs);
+		text = Decompress(&spec, 0, -1, textlen, 0);
+		if (!text || (STR_LEN(text) != textlen)) Crash(RP_BOOT_DATA);
+		boot = Scan_Source(STR_HEAD(text), textlen);
+		//Dump_Block_Raw(boot, 0, 2);
+		Free_Series(text);
+	}
+
 	Set_Root_Series(ROOT_BOOT, boot, "boot block");	// Do not let it get GC'd
-	if (text) Free_Series(text);
 
 	Boot_Block = (BOOT_BLK *)VAL_BLK(BLK_HEAD(boot));
 
@@ -188,15 +187,18 @@ extern const REBYTE Str_Banner[];
 	ASSERT(VAL_WORD_SYM(VAL_BLK(&Boot_Block->types)) == SYM_END_TYPE, RP_BAD_END_TYPE_WORD);
 
 	// Create low-level string pointers (used by RS_ constants):
-	PG_Boot_Strs = (REBYTE **)Make_Mem(RS_MAX * sizeof(REBYTE *));
-	*ROOT_STRINGS = Boot_Block->strings;
-	cp = VAL_BIN(ROOT_STRINGS);
-	for (i = 0; i < RS_MAX; i++) {
-		BOOT_STR(i,0) = cp;
-		while (*cp++);
-	}
+	{
+		REBYTE *cp;
+		REBINT i;
 
-//	*ROOT_SCRIPT = Boot_Block->script;
+		PG_Boot_Strs = (REBYTE **)Make_Mem(RS_MAX * sizeof(REBYTE *));
+		*ROOT_STRINGS = Boot_Block->strings;
+		cp = VAL_BIN(ROOT_STRINGS);
+		for (i = 0; i < RS_MAX; i++) {
+			BOOT_STR(i,0) = cp;
+			while (*cp++);
+		}
+	}
 
 	ASSERT(!CMP_BYTES("end!", Get_Sym_Name(SYM_END_TYPE)), RP_BAD_END_CANON_WORD);
 	ASSERT(!CMP_BYTES("true", Get_Sym_Name(SYM_TRUE)), RP_BAD_TRUE_CANON_WORD);