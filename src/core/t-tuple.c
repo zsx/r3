0c6fcc4aee8458867e844f4cbec850c6734ec687
Shixin Zeng <szeng@atronixengineering.com>
2014-10-02 10:34:44 -0400

    Revert "Fix regressions"
    This reverts commit 59a021cd313d5cdf152f7e2c2233b89ce417a5f7.
    
    Prepare for another fix

--- src/core/t-tuple.c
+++ src/core/t-tuple.c
@@ -184,11 +184,11 @@
 /*
 ***********************************************************************/
 {
-	REBVAL	*value = NULL;
-	REBVAL	*arg = NULL;
-	REBYTE	*vp = NULL;
-	REBYTE	*ap = NULL;
-	REBINT	len = 0;
+	REBVAL	*value;
+	REBVAL	*arg;
+	REBYTE	*vp;
+	REBYTE	*ap;
+	REBINT	len;
 	REBINT	alen;
 	REBINT	v;
 	REBINT	a;
@@ -198,6 +198,8 @@
 	if (IS_TUPLE(value)) {
 		vp = VAL_TUPLE(value);
 		len = VAL_TUPLE_LEN(value);
+	} else if (!(IS_DATATYPE(value) && action == A_MAKE)) {
+		Trap_Arg(value);
 	}
 	arg = D_ARG(2);
 
