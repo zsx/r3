zsx/r3

commit 2d10448789f86445826be1f8dae1f868fae53516

Shut off coverity complains on uninitialized variables

src/core/t-tuple.c
@@ -198,6 +198,8 @@
 	if (IS_TUPLE(value)) {
 		vp = VAL_TUPLE(value);
 		len = VAL_TUPLE_LEN(value);
+	} else if (!(IS_DATATYPE(value) && action == A_MAKE)) {
+		Trap_Arg(value);
 	}
 	arg = D_ARG(2);
 

commit 07f225ba1077f5bc486061fe3f0f8d002279db77

Fix a regression
caused by 2d10448789f86445826be1f8dae1f868fae53516
"Shut off coverity complains on uninitialized variables"

and exposed by
>> to tuple! [1 2 3]

src/core/t-tuple.c
@@ -198,7 +198,7 @@
 	if (IS_TUPLE(value)) {
 		vp = VAL_TUPLE(value);
 		len = VAL_TUPLE_LEN(value);
-	} else if (!(IS_DATATYPE(value) && action == A_MAKE)) {
+	} else if (!(IS_DATATYPE(value) && (action == A_MAKE || action == A_TO))) {
 		Trap_Arg(value);
 	}
 	arg = D_ARG(2);