zsx/r3

commit 8336a45a91d9d737abba742985dc4bf330cd1f8e

Check parameter to avoid uninitialized var
reported by coverity

src/core/s-unicode.c
@@ -935,6 +935,9 @@ ConversionResult ConvertUTF8toUTF32 (
 			if (utf == 8) bp += 3, len -= 3;
 			else if (utf == -16 || utf == 16) bp += 2, len -= 2;
 			else if (utf == -32 || utf == 32) bp += 4, len -= 4;
+			else return NULL;
+		} else {
+			return NULL;
 		}
 	}
 

commit 1d230de812ba6fa60d6eaf18fe9d8d3f40f7ac2d

Fix encoding detection for READ/string
Files with a BOM are now properly handled. Files without a BOM are again
decoded using UTF-8. Trying to READ/string a file with an encoding for
which support is not yet implemented (UCS4 LE and BE) now causes an
error.

This fixes zsx/r3#13, regressions introduced by 8336a45 and 70c7c3a, and
CureCode issue cc#2186.

src/core/p-file.c
@@ -206,7 +206,7 @@ REBINT Mode_Syms[] = {
 	if (args & (AM_READ_STRING | AM_READ_LINES)) {
 		REBSER *nser = Decode_UTF_String(BIN_HEAD(ser), file->actual, -1);
 		if (nser == NULL) {
-			nser = ser;
+			Trap0(RE_BAD_DECODE);
 		}
 		Set_String(ds, nser);
 		if (args & AM_READ_LINES) Set_Block(ds, Split_Lines(ds));

src/core/s-unicode.c
@@ -922,7 +922,9 @@ ConversionResult ConvertUTF8toUTF32 (
 **		Do all the details to decode a string.
 **		Input is a byte series. Len is len of input.
 **		The utf is 0, 8, +/-16, +/-32.
-**		A special -1 means use the BOM.
+**		A special -1 means use the BOM, if present, or UTF-8 otherwise.
+**
+**		Returns the decoded string or NULL for unsupported encodings.
 **
 ***********************************************************************/
 {
@@ -931,17 +933,17 @@ ConversionResult ConvertUTF8toUTF32 (
 	REBINT size;
 
 	if (utf == -1) {
+		// Try to detect UTF encoding from a BOM. Returns 0 if no BOM present.
 		utf = What_UTF(bp, len);
-		if (utf) {
+		if (utf != 0) {
 			if (utf == 8) bp += 3, len -= 3;
 			else if (utf == -16 || utf == 16) bp += 2, len -= 2;
 			//else if (utf == -32 || utf == 32) bp += 4, len -= 4;
 			else return NULL;
-		} else {
-			return NULL;
 		}
 	}
-	else if (utf == 0 || utf == 8) {
+
+	if (utf == 0 || utf == 8) {
 		size = Decode_UTF8((REBUNI*)Reset_Buffer(ser, len), bp, len, TRUE);
 	} 
 	else if (utf == -16 || utf == 16) {
@@ -951,7 +953,8 @@ ConversionResult ConvertUTF8toUTF32 (
 //		size = Decode_UTF32((REBUNI*)Reset_Buffer(ser, len/4 + 1), bp, len, utf < 0, TRUE);
 //	}
 	else {
-		return NULL; /* should never be here */
+		// Encoding is unsupported or not yet implemented.
+		return NULL;
 	}
 
 	if (size < 0) {