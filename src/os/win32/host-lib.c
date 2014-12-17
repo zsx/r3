e6296f02df9a18036d4e417be751121b906d239e
Shixin Zeng <szeng@atronixengineering.com>
2014-12-17 16:14:04 -0500

    Add a native 'access-os'
    It's almost like what's in R2, with the addition of 'euid and 'egid, and
    additional signal number to kill the process:
    
    access-os/set 'pid [2123 9] ;send a signal 9 (SIGKILL) to process 2123

--- src/os/win32/host-lib.c
+++ src/os/win32/host-lib.c
@@ -138,6 +138,156 @@ static void *Task_Ready;
 **
 ***********************************************************************/
 
+/* Keep in sync with n-io.c */
+#define OS_EINVAL -1
+#define OS_EPERM -2
+#define OS_ESRCH -3
+
+/***********************************************************************
+**
+*/	REBINT OS_Get_PID()
+/*
+**		Return the current process ID
+**
+***********************************************************************/
+{
+	return GetCurrentProcessId();
+}
+
+/***********************************************************************
+**
+*/	REBINT OS_Get_UID()
+/*
+**		Return the real user ID
+**
+***********************************************************************/
+{
+	return -1;
+}
+
+/***********************************************************************
+**
+*/	REBINT OS_Set_UID(REBINT uid)
+/*
+**		Set the user ID, see setuid manual for its semantics
+**
+***********************************************************************/
+{
+	return OS_EINVAL;
+}
+
+/***********************************************************************
+**
+*/	REBINT OS_Get_GID()
+/*
+**		Return the real group ID
+**
+***********************************************************************/
+{
+	return -1;
+}
+
+/***********************************************************************
+**
+*/	REBINT OS_Set_GID(REBINT gid)
+/*
+**		Set the group ID, see setgid manual for its semantics
+**
+***********************************************************************/
+{
+	return OS_EINVAL;
+}
+
+/***********************************************************************
+**
+*/	REBINT OS_Get_EUID()
+/*
+**		Return the effective user ID
+**
+***********************************************************************/
+{
+	return -1;
+}
+
+/***********************************************************************
+**
+*/	REBINT OS_Set_EUID(REBINT uid)
+/*
+**		Set the effective user ID
+**
+***********************************************************************/
+{
+	return OS_EINVAL;
+}
+
+/***********************************************************************
+**
+*/	REBINT OS_Get_EGID()
+/*
+**		Return the effective group ID
+**
+***********************************************************************/
+{
+	return -1;
+}
+
+/***********************************************************************
+**
+*/	REBINT OS_Set_EGID(REBINT gid)
+/*
+**		Set the effective group ID
+**
+***********************************************************************/
+{
+	return OS_EINVAL;
+}
+
+/***********************************************************************
+**
+*/	REBINT OS_Send_Signal(REBINT pid, REBINT signal)
+/*
+**		Send signal to a process
+**
+***********************************************************************/
+{
+	return OS_EINVAL;
+}
+
+/***********************************************************************
+**
+*/	REBINT OS_Kill(REBINT pid)
+/*
+**		Try to kill the process
+**
+***********************************************************************/
+{
+	REBINT err = 0;
+	HANDLE ph = OpenProcess(PROCESS_TERMINATE, FALSE, pid);
+	if (ph == NULL) {
+		err = GetLastError();
+		switch (err) {
+			case ERROR_ACCESS_DENIED:
+				return OS_EPERM;
+			case ERROR_INVALID_PARAMETER:
+				return OS_ESRCH;
+			default:
+				return OS_ESRCH;
+		}
+	}
+	if (TerminateProcess(ph, 0)) {
+		CloseHandle(ph);
+		return 0;
+	}
+	err = GetLastError();
+	CloseHandle(ph);
+	switch (err) {
+		case ERROR_INVALID_HANDLE:
+			return OS_EINVAL;
+		default:
+			return -err;
+	}
+}
+
 /***********************************************************************
 **
 */	REBINT OS_Config(int id, REBYTE *result)
