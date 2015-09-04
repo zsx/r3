metaeducation/ren-c

RAISE and PANIC psuedo-keywords
This very large commit is the "Error Renaissance".  It has
several aspects, the most visible of which is that there are two
keyword-like macros called `raise` and `panic` which can be
used to give the appearance of something syntactically like
a throw construct:

    if (!IS_OBJECT(val))
        raise Error_Invalid_Arg(val);

How they work is explained in the comments of sys-core.h.
But the gist is that the crashing or longjmp'ing is not done by
the `raise` or the `panic`, but by the error function.  However
the keyword saves some state that the error function reads,
for instance to know whether to do a raise or panic action.

By combining this with the __attribute((noreturn)) and the
__builtin_unreachable extensions to GCC and Clang, the
uglier tricks using DEAD_END and DEAD_END void are not
necessary.  And the keyword-like syntax serves as a reminder
that something more is going on than just a function call--
that the next line of code will not be executed.

Doing this required unifying the RP_ Rebol Panic codes and
the RE_ Rebol Error codes more clearly.  So there's work in
here that does that, and removes some redundancy of
information that existed between strings.r and errors.r.  The
goal of Panic() is articulated more clearly and it actually acts
more like it seems to have set out to be.

Because every line with an error call on it needed to be
touched, I went ahead and did a light review of all the callsites
and cleaned things up a little bit as I went along.  This meant
shuffling around some of the functions that create errors when
I noticed a number of similar calls, and also removing
DEAD_END or break; instances that were right after a raise
or panic.  Some code formatting adjustments on the lines
above or below an error instantiation.

src/boot/errors.r
@@ -14,19 +14,60 @@ REBOL [
 	}
 ]
 
-Throw: [
+Special: [
 	code: 0
-	type: "non-error"
+	type: "special"		; Not really "errors"
 	null:				{invalid error code zero}
 	halt:               [{halted by user or script}]
+
+	; !!! Temporary: PARSE uses simulated error Trap to get out of arbitrary
+	; stacks when a paren! is THROWN() out of (e.g. parse {} [(return 0)]).
+	; This isn't sensible compared to just having a way to use ordinary
+	; C logic to return out of the stack.  There is overhead to cleaning up
+	; after the longjmp and cost to set up and drop a setjmp state on every
+	; parse (even those which will not need to use this hack).
+	;
+	parse-longjmp-hack:	{RE_PARSE_LONGJMP_HACK not in PARSE (impossible!)}
 ]
 
-Note: [
+Boot: [
+	; These are errors that can happen prior to the point where the error
+	; catalog (e.g. the blocks in this file) have been loaded as Rebol data.
+	; Their strings need to be accessible another way, so %make-boot.r takes
+	; the string data and duplicates it as C literals usable by Panic_Core()
+	;
+	; Note: Because these are flattened into printf-style format strings
+	; instead of using the ordinary error-building mechanism, argument
+	; ordering is not honored.  It's also probably a bad idea to use
+	; sophisticated REBVALs as arguments, because it may be too early
+	; in the boot process for them to be molded properly.
+	;
 	code: 100
-	type: "note"
-	no-load:            [{cannot load: } :arg1]
-	exited:             [{exit occurred}]
-	deprecated:         {deprecated function not allowed}
+	type: "boot"
+	misc:				{RE_MISC error (if actually happens, add to %errors.r)}
+	no-prep:			{raise or panic "keyword" missing from error trigger}
+	no-memory:			[{not enough memory:} :arg1 {bytes}]
+	stack-overflow:		{data stack overflow}
+	io-error:			{problem with IO}
+	max-words:			{too many words}
+	word-list:			{word list (cache) already in use}
+	locked-series:		{locked series expansion}
+	no-saved-state:		{saved state frame is missing}
+	max-events:			{event queue overflow}
+	na:					{not available}
+	unexpected-case		{no case in switch statement}
+	boot-data:			{no boot.r text found}
+	rebval-alignment	{not aligned perfectly in memory}
+	bad-size:			{expected size did not match}
+	no-buffer:			{buffer not yet allocated}
+	bad-boot-string:	{boot strings area is invalid}
+	bad-boot-type-block: {boot block is wrong size}
+	bad-end-type-word:	{the end word is not correct}
+	action-overflow:	{more actions than we should have}
+	native-boot:		{bad boot.r native ordering}
+	bad-end-canon-word:	{END was not found}
+	bad-true-canon-word:	{TRUE was not found}
+	invalid-datatype:	[{invalid datatype #} :arg1]
 ]
 
 Syntax: [
@@ -118,6 +159,7 @@ Script: [
 	parse-command:      [{PARSE - command cannot be used as variable:} :arg1]
 	parse-series:       [{PARSE - input must be a series:} :arg1]
 
+	not-ffi-build:		{This Rebol build wasn't linked with libffi features}
 	bad-library:		{bad library (already closed?)}
 
 ;   bad-prompt:         [{Error executing prompt block}]
@@ -161,7 +203,6 @@ Access: [
 	write-error:        [{write failed:} :arg1 {reason:} :arg2]
 	read-error:         [{read failed:} :arg1 {reason:} :arg2]
 	read-only:          [{read-only - write not allowed:} :arg1]
-	no-buffer:          [{port has no data buffer:} :arg1]
 	timeout:            [{port action timed out:} :arg1]
 
 	no-create:          [{cannot create:} :arg1]
@@ -212,32 +253,15 @@ Command: [
 	command-fail:		["Command failed"]
 ]
 
-resv700: [
-	code: 700
-	type: "reserved"
-	; !!! Temporary: PARSE uses simulated error Trap to get out of arbitrary
-	; stacks when a paren! is THROWN() out of (e.g. parse {} [(return 0)]).
-	; This isn't sensible compared to just having a way to use ordinary
-	; C logic to return out of the stack.  There is overhead to cleaning up
-	; after the longjmp and cost to set up and drop a setjmp state on every
-	; parse (even those which will not need to use this hack).
-	parse-longjmp-hack:	{RE_PARSE_LONGJMP_HACK not in PARSE (impossible!)}
-]
+; 700-799 unused
 
-User: [
-	code: 800
-	type: "user error"
-	message: [:arg1]
-]
+; 800-899 unused
 
 Internal: [
 	code: 900
 	type: "internal error"
-	misc:				[{miscellaneous error (add specific case to errors.r)}]
 	bad-path:           [{bad path:} arg1]
 	not-here:           [arg1 {not supported on your system}]
-	no-memory:          {not enough memory}
-	stack-overflow:     {stack overflow}
 	globals-full:       {no more global variable space}
 	max-natives:        {too many natives}
 	bad-series:         {invalid series}
@@ -246,4 +270,15 @@ Internal: [
 	feature-na:         {feature not available}
 	not-done:           {reserved for future use (or not yet implemented)}
 	invalid-error:      {error object or fields were not valid}
+	bad-evaltype:		{invalid datatype for evaluation}
+	corrupt-memory:		{Check_Memory() found a problem}
+	hash-overflow:		{Hash ran out of space}
+	no-print-ptr:		{print is missing string pointer}
 ]
+
+User: [
+	code: 1000
+	type: "user error"
+	message: [:arg1]
+]
+

src/core/d-crash.c
@@ -29,112 +29,127 @@
 
 #include "sys-core.h"
 
-#define	PANIC_BUF_SIZE 512	// space for crash print string
-
-enum Panic_Msg_Nums {
-	// Must align with Panic_Msgs[] array.
-	CM_ERROR,
-	CM_BOOT,
-	CM_INTERNAL,
-	CM_DATATYPE,
-	CM_DEBUG,
-	CM_CONTACT
-};
+// Size of crash buffers
+#define	PANIC_TITLE_SIZE 80
+#define	PANIC_MESSAGE_SIZE 512
 
 
 /***********************************************************************
 **
-** coverity[+kill]
-*/	void Panic_Core(REBINT id, ...)
+*/	ATTRIBUTE_NO_RETURN void Panic_Core(REBINT id, const REBVAL *maybe_error, const char *c_file, int c_line, va_list *args)
 /*
-**		Print a failure message and abort.
+**		(va_list by pointer: http://stackoverflow.com/a/3369762/211160)
 **
-**		LATIN1 ONLY!! (For now)
-**
-**		The error is identified by id number, which can reference an
-**		error message string in the boot strings block.
-**
-**		Note that lower level error messages should not attempt to
-**		use the %r (mold value) format (uses higher level functions).
-**
-**		See panics.h for list of crash errors.
+**		Print a failure message and abort.  The code adapts to several
+**		different load stages of the system, and uses simpler ways to
+**		report the error when the boot has not progressed enough to
+**		use the more advanced modes.  This allows the same interface
+**		to be used for `panic Error_XXX(...)` and `raise Error_XXX(...)`.
 **
 ***********************************************************************/
 {
-	va_list args;
-	char buf[PANIC_BUF_SIZE];
-	const char *msg;
-	REBINT n = 0;
+	char title[PANIC_TITLE_SIZE];
+	char message[PANIC_MESSAGE_SIZE];
 
-	va_start(args, id);
+	title[0] = '\0';
+	message[0] = '\0';
 
-	// We are crashing so something is internally wrong...a legitimate
-	// time to be disabling the garbage collector.
-	//
-	// !!! But should we be doing FORMing etc. to make a message from
-	// varargs or just spitting out the crash in a less risky way?  Is
-	// Panic overdesigned for its purpose?
+	// We are crashing, so a legitimate time to be disabling the garbage
+	// collector.  (It won't be turned back on.)
 	GC_Disabled++;
 
-	if (Reb_Opts->crash_dump) {
+	if (Reb_Opts && Reb_Opts->crash_dump) {
 		Dump_Info();
 		Dump_Stack(0, 0);
 	}
 
-	// "REBOL PANIC #nnn:"
-	strncpy(buf, Panic_Msgs[CM_ERROR], PANIC_BUF_SIZE);
-	buf[PANIC_BUF_SIZE - 1] = '\0';
-	strncat(buf, " #", PANIC_BUF_SIZE);
-	Form_Int(b_cast(buf + strlen(buf)), id);
-	strncat(buf, ": ", PANIC_BUF_SIZE);
+	strncat(title, "PANIC #", PANIC_TITLE_SIZE - 1);
+	Form_Int(b_cast(title + strlen(title)), id); // !!! no bounding...
 
-	// "REBOL PANIC #nnn: put error message here"
-	// The first few error types only print general error message.
-	// Those errors > RP_STR_BASE have specific error messages (from boot.r).
-	if      (id < RP_BOOT_DATA) n = CM_DEBUG;
-	else if (id < RP_INTERNAL) n = CM_BOOT;
-	else if (id < RP_DATATYPE)  n = CM_INTERNAL;
-	else if (id < RP_STR_BASE) n = CM_DATATYPE;
-	else if (id > RP_STR_BASE + RS_MAX - RS_ERROR) n = CM_DEBUG;
+	strncat(message, Str_Panic_Directions, PANIC_MESSAGE_SIZE - 1);
 
-	// Use the above string or the boot string for the error (in boot.r):
-	msg = n >= 0 ? Panic_Msgs[n] : cs_cast(BOOT_STR(RS_ERROR, id - RP_STR_BASE - 1));
-	Form_Var_Args(
-		b_cast(buf + strlen(buf)), PANIC_BUF_SIZE - 1 - strlen(buf), msg, &args
+#if !defined(NDEBUG)
+	// In debug builds, we have the file and line number to report
+	Form_Args(
+		b_cast(message + strlen(message)),
+		PANIC_MESSAGE_SIZE - 1 - strlen(message),
+		"C Source File %s, Line %d\n",
+		c_file,
+		c_line,
+		NULL
 	);
-
-	va_end(args);
-
-	strncat(buf, Panic_Msgs[CM_CONTACT], PANIC_BUF_SIZE - 1);
-
-	// Convert to OS-specific char-type:
-#ifdef disable_for_now //OS_WIDE_CHAR   /// win98 does not support it
-	{
-		REBCHR s1[512];
-		REBCHR s2[2000];
-
-		n = OS_STRNCPY(s1, Panic_Msgs[CM_ERROR], LEN_BYTES(Panic_Msgs[CM_ERROR]));
-		if (n > 0) s1[n] = 0; // terminate
-		else OS_EXIT(200); // bad conversion
-
-		n = OS_STRNCPY(s2, buf, LEN_BYTES(buf));
-		if (n > 0) s2[n] = 0;
-		else OS_EXIT(200);
-
-		OS_CRASH(s1, s2);
-	}
-#else
-	OS_CRASH(cb_cast(Panic_Msgs[CM_ERROR]), cb_cast(buf));
 #endif
+
+	if (PG_Boot_Phase < BOOT_LOADED) {
+		strncat(message, title, PANIC_MESSAGE_SIZE - 1);
+		strncat(
+			message,
+			"\n** Boot Error: (string table not decompressed yet)",
+			PANIC_MESSAGE_SIZE - 1
+		);
+	}
+	else if (PG_Boot_Phase < BOOT_ERRORS && id < RE_BOOT_MAX) {
+		// We are panic'ing on one of the errors that can occur during
+		// boot (e.g. before Make_Error() be assured to run).  So we use
+		// the C string constant that was formed by %make-boot.r and
+		// compressed in the boot block.
+		//
+		// Note: These strings currently do not allow arguments.
+
+		const char *format = cs_cast(BOOT_STR(RS_ERROR, id - RE_BOOT_FIRST));
+		assert(args && !maybe_error);
+		strncat(message, "\n** Boot Error: ", PANIC_MESSAGE_SIZE - 1);
+		Form_Args_Core(
+			b_cast(message + strlen(message)),
+			PANIC_MESSAGE_SIZE - 1 - strlen(message),
+			format,
+			args
+		);
+	}
+	else if (PG_Boot_Phase < BOOT_ERRORS && id >= RE_BOOT_MAX) {
+		strncat(message, title, PANIC_MESSAGE_SIZE - 1);
+		strncat(
+			message,
+			"\n** Boot Error: (error object table not initialized yet)",
+			PANIC_MESSAGE_SIZE - 1
+		);
+	}
+	else {
+		// The system should be theoretically able to make and mold errors.
+		//
+		// !!! If you're trying to panic *during* error molding this
+		// is obviously not going to not work.  All errors pertaining to
+		// molding errors should audited to be in the Boot: category.
+
+		REBVAL error;
+
+		if (maybe_error) {
+			assert(!args);
+			error = *maybe_error;
+		}
+		else {
+			// We aren't explicitly passed a Rebol ERROR! object, but we
+			// consider it "safe" to make one since we're past BOOT_ERRORS
+
+			Val_Init_Error(&error, Make_Error_Core(id, c_file, c_line, args));
+		}
+
+		Form_Args(
+			b_cast(message + strlen(message)),
+			PANIC_MESSAGE_SIZE - 1 - strlen(message),
+			"%v",
+			&error,
+			NULL
+		);
+	}
+
+	OS_CRASH(cb_cast(Str_Panic_Title), cb_cast(message));
+
+	// Note that since we crash, we never return so that the caller can run
+	// a va_end on the passed-in args.  This is illegal in the general case:
+	//
+	//    http://stackoverflow.com/a/587139/211160
+
+	DEAD_END;
 }
 
-/***********************************************************************
-**
-*/	void NA(void)
-/*
-**		Feature not available.
-**
-***********************************************************************/
-{
-	Panic(RP_NA);
-}

zsx/r3

src/boot/errors.r
@@ -14,19 +14,95 @@ REBOL [
 	}
 ]
 
-Throw: [
+Special: [
 	code: 0
-	type: "non-error"
+	type: "special"		; Not really "errors"
 	null:				{invalid error code zero}
 	halt:               {halted by user or script}
 	exited:             {exit occurred}
+
+	; !!! Temporary: PARSE uses simulated error Trap to get out of arbitrary
+	; stacks when a paren! is THROWN() out of (e.g. parse {} [(return 0)]).
+	; This isn't sensible compared to just having a way to use ordinary
+	; C logic to return out of the stack.  There is overhead to cleaning up
+	; after the longjmp and cost to set up and drop a setjmp state on every
+	; parse (even those which will not need to use this hack).
+	;
+	parse-longjmp-hack:	{RE_PARSE_LONGJMP_HACK not in PARSE (impossible!)}
 ]
 
-Note: [
+Internal: [
+	; Some of these internal errors can happen prior to when the error
+	; catalog (e.g. the blocks in this file) have been loaded as Rebol data.
+	; Those strings need to be accessible another way, so %make-boot.r takes
+	; the string data and duplicates it as C literals usable by Panic_Core()
+	;
+	; Note: Because these are flattened into printf-style format strings
+	; instead of using the ordinary error-building mechanism, argument
+	; ordering is not honored when an error is raised using the mechanism
+	; from before when booting is ready.  It's also probably a bad idea to
+	; use sophisticated REBVALs as arguments, because it may be too early
+	; in the boot process for them to be molded properly.
+	;
 	code: 100
-	type: "note"
-	no-load:            [{cannot load: } :arg1]
-	deprecated:         {deprecated function not allowed}
+	type: "internal"
+
+	; Because adding an error code has the overhead of modifying this file and
+	; coming up with a good name, errors for testing or that you don't think
+	; will happen can be put here.  A debug build will identify the line
+	; number and file source of the error, so provides some info already.
+	;
+	misc:				{RE_MISC error (if actually happens, add to %errors.r)}
+
+	; The error machinery expects a `raise` or `panic` macro to have signaled
+	; whether the error is meant to be trappable or to exit the process
+	;
+	no-prep:			{raise or panic "keyword" missing from error trigger}
+
+	; !!! Should there be a distinction made between different kinds of
+	; stack overflows?  (Call stack, Data stack?)
+	;
+	stack-overflow:		{stack overflow}
+
+	not-done:           {reserved for future use (or not yet implemented)}
+
+	; !!! Should boot errors be their own category?  Even if they were,
+	; there will still be some errors that are not boot-specific which can
+	; happen during boot (such as out of memory) which can't rely on boot
+	;
+	no-memory:			[{not enough memory:} :arg1 {bytes}]
+	corrupt-memory:		{Check_Memory() found a problem}
+	boot-data:			{no boot.r text found}
+	native-boot:		{bad boot.r native ordering}
+	bad-boot-string:	{boot strings area is invalid}
+	bad-boot-type-block: {boot block is wrong size}
+	max-natives:        {too many natives}
+	action-overflow:	{more actions than we should have}
+	bad-end-canon-word:	{END was not found}
+	bad-true-canon-word:	{TRUE was not found}
+	bad-end-type-word:	{the end word is not correct}
+	rebval-alignment	{sizeof(REBVAL) not 4x 32-bits or 4x 64-bits}
+
+	io-error:			{problem with IO}
+	max-words:			{too many words}
+	word-list:			{word list (cache) already in use}
+	locked-series:		{locked series expansion}
+	no-saved-state:		{saved state frame is missing}
+	max-events:			{event queue overflow}
+	unexpected-case		{no case in switch statement}
+	bad-size:			{expected size did not match}
+	no-buffer:			{buffer not yet allocated}
+	invalid-datatype:	[{invalid datatype #} :arg1]
+	bad-path:           [{bad path:} :arg1]
+	not-here:           [:arg1 {not supported on your system}]
+	globals-full:       {no more global variable space}
+	bad-series:         {invalid series}
+	limit-hit:          [{internal limit reached:} :arg1]
+	bad-sys-func:       [{invalid or missing system function:} :arg1]
+	invalid-error:      {error object or fields were not valid}
+	bad-evaltype:		{invalid datatype for evaluation}
+	hash-overflow:		{Hash ran out of space}
+	no-print-ptr:		{print is missing string pointer}
 ]
 
 Syntax: [
@@ -118,6 +194,7 @@ Script: [
 	parse-command:      [{PARSE - command cannot be used as variable:} :arg1]
 	parse-series:       [{PARSE - input must be a series:} :arg1]
 
+	not-ffi-build:		{This Rebol build wasn't linked with libffi features}
 	bad-library:		{bad library (already closed?)}
 
 ;   bad-prompt:         [{Error executing prompt block}]
@@ -153,7 +230,7 @@ Access: [
 	invalid-spec:       [{invalid spec or options:} :arg1]
 	invalid-port:       [{invalid port object (invalid field values)}]
 	invalid-actor:      [{invalid port actor (must be native or object)}]
-	invalid-port-arg:   [{invalid port argument:} arg1]
+	invalid-port-arg:   [{invalid port argument:} :arg1]
 	no-port-action:     [{this port does not support:} :arg1]
 	protocol:           [{protocol error:} :arg1]
 	invalid-check:      [{invalid checksum (tampered file):} :arg1]
@@ -161,7 +238,6 @@ Access: [
 	write-error:        [{write failed:} :arg1 {reason:} :arg2]
 	read-error:         [{read failed:} :arg1 {reason:} :arg2]
 	read-only:          [{read-only - write not allowed:} :arg1]
-	no-buffer:          [{port has no data buffer:} :arg1]
 	timeout:            [{port action timed out:} :arg1]
 
 	no-create:          [{cannot create:} :arg1]
@@ -212,38 +288,15 @@ Command: [
 	command-fail:		["Command failed"]
 ]
 
-resv700: [
-	code: 700
-	type: "reserved"
-	; !!! Temporary: PARSE uses simulated error Trap to get out of arbitrary
-	; stacks when a paren! is THROWN() out of (e.g. parse {} [(return 0)]).
-	; This isn't sensible compared to just having a way to use ordinary
-	; C logic to return out of the stack.  There is overhead to cleaning up
-	; after the longjmp and cost to set up and drop a setjmp state on every
-	; parse (even those which will not need to use this hack).
-	parse-longjmp-hack:	{RE_PARSE_LONGJMP_HACK not in PARSE (impossible!)}
-]
+; 700-799 unused
+
+; 800-899 unused
+
+; 900-999 unused
 
 User: [
-	code: 800
+	code: 1000
 	type: "user error"
 	message: [:arg1]
 ]
 
-Internal: [
-	code: 900
-	type: "internal error"
-	misc:				[{miscellaneous error (add specific case to errors.r)}]
-	bad-path:           [{bad path:} arg1]
-	not-here:           [arg1 {not supported on your system}]
-	no-memory:          {not enough memory}
-	stack-overflow:     {stack overflow}
-	globals-full:       {no more global variable space}
-	max-natives:        {too many natives}
-	bad-series:         {invalid series}
-	limit-hit:          [{internal limit reached:} :arg1]
-	bad-sys-func:       [{invalid or missing system function:} :arg1]
-	feature-na:         {feature not available}
-	not-done:           {reserved for future use (or not yet implemented)}
-	invalid-error:      {error object or fields were not valid}
-]

src/core/d-crash.c
@@ -29,112 +29,128 @@
 
 #include "sys-core.h"
 
-#define	PANIC_BUF_SIZE 512	// space for crash print string
-
-enum Panic_Msg_Nums {
-	// Must align with Panic_Msgs[] array.
-	CM_ERROR,
-	CM_BOOT,
-	CM_INTERNAL,
-	CM_DATATYPE,
-	CM_DEBUG,
-	CM_CONTACT
-};
+// Size of crash buffers
+#define	PANIC_TITLE_SIZE 80
+#define	PANIC_MESSAGE_SIZE 512
 
 
 /***********************************************************************
 **
-** coverity[+kill]
-*/	void Panic_Core(REBINT id, ...)
+*/	ATTRIBUTE_NO_RETURN void Panic_Core(REBINT id, const REBVAL *maybe_error, const char *c_file, int c_line, va_list *args)
 /*
-**		Print a failure message and abort.
+**		(va_list by pointer: http://stackoverflow.com/a/3369762/211160)
 **
-**		LATIN1 ONLY!! (For now)
-**
-**		The error is identified by id number, which can reference an
-**		error message string in the boot strings block.
-**
-**		Note that lower level error messages should not attempt to
-**		use the %r (mold value) format (uses higher level functions).
-**
-**		See panics.h for list of crash errors.
+**		Print a failure message and abort.  The code adapts to several
+**		different load stages of the system, and uses simpler ways to
+**		report the error when the boot has not progressed enough to
+**		use the more advanced modes.  This allows the same interface
+**		to be used for `panic Error_XXX(...)` and `raise Error_XXX(...)`.
 **
 ***********************************************************************/
 {
-	va_list args;
-	char buf[PANIC_BUF_SIZE];
-	const char *msg;
-	REBINT n = 0;
+	char title[PANIC_TITLE_SIZE];
+	char message[PANIC_MESSAGE_SIZE];
 
-	va_start(args, id);
+	title[0] = '\0';
+	message[0] = '\0';
 
-	// We are crashing so something is internally wrong...a legitimate
-	// time to be disabling the garbage collector.
-	//
-	// !!! But should we be doing FORMing etc. to make a message from
-	// varargs or just spitting out the crash in a less risky way?  Is
-	// Panic overdesigned for its purpose?
+	// We are crashing, so a legitimate time to be disabling the garbage
+	// collector.  (It won't be turned back on.)
 	GC_Disabled++;
 
-	if (Reb_Opts->crash_dump) {
+	if (Reb_Opts && Reb_Opts->crash_dump) {
 		Dump_Info();
 		Dump_Stack(0, 0);
 	}
 
-	// "REBOL PANIC #nnn:"
-	strncpy(buf, Panic_Msgs[CM_ERROR], PANIC_BUF_SIZE);
-	buf[PANIC_BUF_SIZE - 1] = '\0';
-	strncat(buf, " #", PANIC_BUF_SIZE);
-	Form_Int(b_cast(buf + strlen(buf)), id);
-	strncat(buf, ": ", PANIC_BUF_SIZE);
+	strncat(title, "PANIC #", PANIC_TITLE_SIZE - 1);
+	Form_Int(b_cast(title + strlen(title)), id); // !!! no bounding...
 
-	// "REBOL PANIC #nnn: put error message here"
-	// The first few error types only print general error message.
-	// Those errors > RP_STR_BASE have specific error messages (from boot.r).
-	if      (id < RP_BOOT_DATA) n = CM_DEBUG;
-	else if (id < RP_INTERNAL) n = CM_BOOT;
-	else if (id < RP_DATATYPE)  n = CM_INTERNAL;
-	else if (id < RP_STR_BASE) n = CM_DATATYPE;
-	else if (id > RP_STR_BASE + RS_MAX - RS_ERROR) n = CM_DEBUG;
+	strncat(message, Str_Panic_Directions, PANIC_MESSAGE_SIZE - 1);
 
-	// Use the above string or the boot string for the error (in boot.r):
-	msg = n >= 0 ? Panic_Msgs[n] : cs_cast(BOOT_STR(RS_ERROR, id - RP_STR_BASE - 1));
-	Form_Var_Args(
-		b_cast(buf + strlen(buf)), PANIC_BUF_SIZE - 1 - strlen(buf), msg, &args
+#if !defined(NDEBUG)
+	// In debug builds, we have the file and line number to report
+	Form_Args(
+		b_cast(message + strlen(message)),
+		PANIC_MESSAGE_SIZE - 1 - strlen(message),
+		"C Source File %s, Line %d\n",
+		c_file,
+		c_line,
+		NULL
 	);
-
-	va_end(args);
-
-	strncat(buf, Panic_Msgs[CM_CONTACT], PANIC_BUF_SIZE - 1);
-
-	// Convert to OS-specific char-type:
-#ifdef disable_for_now //OS_WIDE_CHAR   /// win98 does not support it
-	{
-		REBCHR s1[512];
-		REBCHR s2[2000];
-
-		n = OS_STRNCPY(s1, Panic_Msgs[CM_ERROR], LEN_BYTES(Panic_Msgs[CM_ERROR]));
-		if (n > 0) s1[n] = 0; // terminate
-		else OS_EXIT(200); // bad conversion
-
-		n = OS_STRNCPY(s2, buf, LEN_BYTES(buf));
-		if (n > 0) s2[n] = 0;
-		else OS_EXIT(200);
-
-		OS_CRASH(s1, s2);
-	}
-#else
-	OS_CRASH(cb_cast(Panic_Msgs[CM_ERROR]), cb_cast(buf));
 #endif
+
+	if (PG_Boot_Phase < BOOT_LOADED) {
+		strncat(message, title, PANIC_MESSAGE_SIZE - 1);
+		strncat(
+			message,
+			"\n** Boot Error: (string table not decompressed yet)",
+			PANIC_MESSAGE_SIZE - 1
+		);
+	}
+	else if (PG_Boot_Phase < BOOT_ERRORS && id < RE_INTERNAL_MAX) {
+		// We are panic'ing on one of the errors that can occur during
+		// boot (e.g. before Make_Error() be assured to run).  So we use
+		// the C string constant that was formed by %make-boot.r and
+		// compressed in the boot block.
+		//
+		// Note: These strings currently do not allow arguments.
+
+		const char *format =
+			cs_cast(BOOT_STR(RS_ERROR, id - RE_INTERNAL_FIRST));
+		assert(args && !maybe_error);
+		strncat(message, "\n** Boot Error: ", PANIC_MESSAGE_SIZE - 1);
+		Form_Args_Core(
+			b_cast(message + strlen(message)),
+			PANIC_MESSAGE_SIZE - 1 - strlen(message),
+			format,
+			args
+		);
+	}
+	else if (PG_Boot_Phase < BOOT_ERRORS && id >= RE_INTERNAL_MAX) {
+		strncat(message, title, PANIC_MESSAGE_SIZE - 1);
+		strncat(
+			message,
+			"\n** Boot Error: (error object table not initialized yet)",
+			PANIC_MESSAGE_SIZE - 1
+		);
+	}
+	else {
+		// The system should be theoretically able to make and mold errors.
+		//
+		// !!! If you're trying to panic *during* error molding this
+		// is obviously not going to not work.  All errors pertaining to
+		// molding errors should audited to be in the Boot: category.
+
+		REBVAL error;
+
+		if (maybe_error) {
+			assert(!args);
+			error = *maybe_error;
+		}
+		else {
+			// We aren't explicitly passed a Rebol ERROR! object, but we
+			// consider it "safe" to make one since we're past BOOT_ERRORS
+
+			Val_Init_Error(&error, Make_Error_Core(id, c_file, c_line, args));
+		}
+
+		Form_Args(
+			b_cast(message + strlen(message)),
+			PANIC_MESSAGE_SIZE - 1 - strlen(message),
+			"%v",
+			&error,
+			NULL
+		);
+	}
+
+	OS_CRASH(cb_cast(Str_Panic_Title), cb_cast(message));
+
+	// Note that since we crash, we never return so that the caller can run
+	// a va_end on the passed-in args.  This is illegal in the general case:
+	//
+	//    http://stackoverflow.com/a/587139/211160
+
+	DEAD_END;
 }
 
-/***********************************************************************
-**
-*/	void NA(void)
-/*
-**		Feature not available.
-**
-***********************************************************************/
-{
-	Panic(RP_NA);
-}