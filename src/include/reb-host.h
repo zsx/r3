9bef568304e20ce35737eee09fab7dfe610694dd
Hostile Fork <brian@hostilefork.com>
2015-09-24 16:21:10 -0400

    Add <stdlib.h> to the basics included before host-lib.h
    There's no formal baseline for what is legal in parameter types for
    host library functions... but the allocator uses size_t.  Since that is
    a dependency, it means that code using host-lib.h won't compile
    unless size_t is defined.
    
    This decision could be revisited to use some other type, e.g.
    REBCNT or REBU64.  But given that the idea of the host kit is
    that the protocols are compiled with Rebol natively (e.g. routines
    using either wchar_t or char), that decision should probably be
    made when the host-and-Rebol protocols are nailed down in
    other ways based on what the rules are.

--- src/include/reb-host.h
+++ src/include/reb-host.h
@@ -52,4 +52,11 @@
 #include "reb-gob.h"
 #include "reb-lib.h"
 
+// !!! None of the above currently include anything that *necessarily* defines
+// size_t.  However the host-lib API currently uses it in defining its
+// allocator.  In order to match the signature of Alloc_Mem() and malloc(),
+// we include it for the moment, but a more formal policy decision on "what
+// parameter types are legal in the host API" would be ideal.
+#include <stdlib.h>
+
 #include "host-lib.h"
