zsx/r3

commit 21ee09b2dc538702757a58bc735509564d571a39

Simplify AJOIN native to not use data stack
The AJOIN native used an unnecessarily complex process of
accumulating all the reduced values onto the data stack and then
producing a mold series from it.  There is no reason why it couldn't just
process each value as it got to it.

src/core/n-strings.c
@@ -124,21 +124,20 @@ static struct digest {
 /*
 ***********************************************************************/
 {
-	REBSER *str;
+	REBSER *block = VAL_SERIES(D_ARG(1));
+	REBCNT index = VAL_INDEX(D_ARG(1));
 
-	// !!! This uses stack pushing/popping protocol and doesn't need
-	// to.  It's only called once; should probably be inlined here.
-	// (Assuming AJOIN should exist, which it probably shouldn't.)
+	REB_MOLD mo;
+	CLEARS(&mo);
+	Reset_Mold(&mo);
 
-	str = Form_Reduce(VAL_SERIES(D_ARG(1)), VAL_INDEX(D_ARG(1)));
-	if (!str) {
-		DS_POP_INTO(D_OUT);
-		return R_OUT;
+	while (index != END_FLAG) {
+		index = Do_Next_May_Throw(D_OUT, block, index);
+		if (index == THROWN_FLAG) return R_OUT;
+		Mold_Value(&mo, D_OUT, 0);
 	}
 
-	DS_DROP;
-
-	Val_Init_String(D_OUT, str); // not D_OUT (stack modified)
+	Val_Init_String(D_OUT, Copy_String(mo.series, 0, -1));
 
 	return R_OUT;
 }

src/core/s-mold.c
@@ -1299,42 +1299,6 @@ append:
 
 /***********************************************************************
 **
-*/	REBSER *Form_Reduce(REBSER *block, REBCNT index)
-/*
-**		Reduce a block and then form each value into a string. Return the
-**		string or NULL if an unwind triggered while reducing.
-**
-***********************************************************************/
-{
-	REBINT start = DSP + 1;
-	REBINT n;
-	REBSER *result = NULL;
-
-	REB_MOLD mo;
-
-	while (index < BLK_LEN(block)) {
-		REBVAL out;
-		index = Do_Next_May_Throw(&out, block, index);
-		if (index == THROWN_FLAG) goto return_balanced;
-		DS_PUSH(&out);
-	}
-
-	CLEARS(&mo);
-	Reset_Mold(&mo);
-
-	for (n = start; n <= DSP; n++)
-		Mold_Value(&mo, DS_AT(n), 0);
-
-	result = Copy_String(mo.series, 0, -1);
-
-return_balanced:
-	DS_DROP_TO(start);
-	return result;
-}
-
-
-/***********************************************************************
-**
 */  REBSER *Form_Tight_Block(const REBVAL *blk)
 /*
 ***********************************************************************/

commit 19be494a0b4ce6f11b47ea6b194c93fee649e86f

Fix bug introduced to AJOIN on nested molds
Commit 21ee09b introduced a bug, when it seemed that there was
an unnecessary use of the data stack.  The data stack was not needed
for protection of the values from GC it turns out...but rather to avoid a
potential recursive use of the MOLD buffer.

This resolves the issue, comments it, and modifies the code to be slightly
more obvious.

src/core/n-strings.c
@@ -124,20 +124,8 @@ static struct digest {
 /*
 ***********************************************************************/
 {
-	REBSER *block = VAL_SERIES(D_ARG(1));
-	REBCNT index = VAL_INDEX(D_ARG(1));
-
-	REB_MOLD mo;
-	CLEARS(&mo);
-	Reset_Mold(&mo);
-
-	while (index != END_FLAG) {
-		index = Do_Next_May_Throw(D_OUT, block, index);
-		if (index == THROWN_FLAG) return R_OUT_IS_THROWN;
-		Mold_Value(&mo, D_OUT, 0);
-	}
-
-	Val_Init_String(D_OUT, Copy_String(mo.series, 0, -1));
+	if (Form_Reduce_Throws(D_OUT, VAL_SERIES(D_ARG(1)), VAL_INDEX(D_ARG(1))))
+		return R_OUT_IS_THROWN;
 
 	return R_OUT;
 }

src/core/s-mold.c
@@ -1299,6 +1299,50 @@ append:
 
 /***********************************************************************
 **
+*/	REBFLG Form_Reduce_Throws(REBVAL *out, REBSER *block, REBCNT index)
+/*
+**		Reduce a block and then form each value into a string REBVAL.
+**
+***********************************************************************/
+{
+	REBINT start = DSP;
+	REBINT n;
+
+	REB_MOLD mo;
+
+	// Reducing all the items to the data stack before molding is necessary
+	// in order to use the single task-local mold buffer.  Initializing the
+	// mold buffer here and calling it on each evaluation might overwrite it
+	// if the called function does any molding.
+	//
+	// !!! Should the mold buffer be treated as a "mold stack?" to avoid
+	// this problem, while still not needing to allocate more than one buffer
+	// per thread?
+
+	while (index < BLK_LEN(block)) {
+		index = Do_Next_May_Throw(out, block, index);
+		if (index == THROWN_FLAG) {
+			DS_DROP_TO(start);
+			return TRUE;
+		}
+		DS_PUSH(out);
+	}
+
+	CLEARS(&mo);
+	Reset_Mold(&mo);
+
+	for (n = start + 1; n <= DSP; n++)
+		Mold_Value(&mo, DS_AT(n), 0);
+
+	DS_DROP_TO(start);
+
+	Val_Init_String(out, Copy_String(mo.series, 0, -1));
+	return FALSE;
+}
+
+
+/***********************************************************************
+**
 */  REBSER *Form_Tight_Block(const REBVAL *blk)
 /*
 ***********************************************************************/