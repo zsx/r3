ed0a5ce47429d49fbe542c93ad8666210e2ce6b5
Shixin Zeng <szeng@atronixengineering.com>
2015-11-23 17:24:50 -0500

    [AGG] Fix multiple grad-pens in one draw block
    Think of the following draw commands (psedu code):
    
    grad-pen grad1
    box ...
    grad-pen grad2
    grad-pen grad3
    box ...
    
    When agg_gradient_pen is called on grad2, box is not drewn yet, it was
    only queued into m_attributes array, so m_color_profile (same as grad1)
    can't be deleted. While it's processing grad3, as m_color_profile will
    be pointing to grad2, which is not referenced by anyone, it has to be
    deleted or it will have a memory leak.
    
    If m_color_profile is being referenced anywhere, it's gotta be the last
    attribute, so just checking the equality will tell if it needs to be
    deleted before creating new storage.

--- src/agg/agg_graphics.cpp
+++ src/agg/agg_graphics.cpp
@@ -1303,8 +1303,14 @@ Reb_Print(
             default:
                 return;
 		}
-		if (m_color_profile == NULL)
-			m_color_profile = new rgba8 [256];
+
+		if (m_color_profile
+			&& m_attributes[m_attributes.size() - 1].colors != m_color_profile) {
+			//printf("deleting unreferenced m_color_profile: %p\n", m_color_profile);
+			delete [] m_color_profile;
+		}
+
+		m_color_profile = new rgba8 [256];
 		unsigned num_pnt = colors[0] - 1;
 
 		unsigned o = 3, of = 2, j = 5;
