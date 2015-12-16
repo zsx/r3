5dd4643ca8c748c3fdfbefcfcb2e0608b56e32a4
Shixin Zeng <szeng@atronixengineering.com>
2015-12-16 10:27:24 -0500

    Fix mezz function ARRAY
    The refinement argument is unset! when the refinement is not requested:
    
    >> array 5
    ** Script error: value has no value
    ** Where: case array
    ** Near: block: make block! size case [
        block? :rest [
                lo...
    
    ** Note: use WHY? for more error information

--- src/mezz/mezz-series.r
+++ src/mezz/mezz-series.r
@@ -114,9 +114,9 @@ array: func [
         any-function? :value [ ; So value can be a thunk :)
             loop size [block: insert/only block value] ; Called every time
         ]
-        true [
-            insert/dup block value size
-        ]
+	true [
+            insert/dup block either initial [value][none] size
+	]
     ]
     head block
 ]
