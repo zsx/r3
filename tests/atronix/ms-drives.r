1a5428c995f221904b996201a9e2b50e27ead3be
Shixin Zeng <szeng@atronixengineering.com>
2017-05-04 16:25:46 -0400

    Fix syntax errors in ms-drives.r

--- tests/atronix/ms-drives.r
+++ tests/atronix/ms-drives.r
@@ -1,16 +1,15 @@
 REBOL []
 
 msvcrt: make library! %msvcrt.dll
-getdrives: make routine! compose/deep [
-    [return: [uint32]]
-    (msvcrt) "_getdrives"
+getdrives: make-routine msvcrt "_getdrives" compose/deep [
+    return: [uint32]
 ]
 
 maps: getdrives
 i: 0
 while [i < 26] [
     unless zero? maps and* shift 1 i [
-        print rejoin [to char! (to integer! #"A") + i ":"]
+        print unspaced [to char! (to integer! #"A") + i ":"]
     ]
     ++ i
 ]
