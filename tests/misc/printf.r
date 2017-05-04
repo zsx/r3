fab2b19faab943f55ef6e188d4d10b1b5786ea9d
Shixin Zeng <szeng@atronixengineering.com>
2017-05-04 16:04:08 -0400

    Fix the syntax error in printf.r
    Also added some more tests

--- tests/misc/printf.r
+++ tests/misc/printf.r
@@ -7,13 +7,13 @@ libc: make library! %libc.so.6
 
 x64?: 40 = fifth system/version
 size_t: either x64? ['int64]['int32]
-printf: make-routine compose/deep [
-    [
-        f [pointer]
-        ...
-    ]
-    (libc) "printf"
+printf: make-routine libc "printf" compose/deep [
+    return: [int32]
+    f [pointer]
+    ...
 ]
 
-printf ["hello"]
-print "hi"
+;(printf "hello^/" 0 [pointer])
+(printf "hello %s^/" "world" [pointer])
+(printf "hello^/")
+(print "hi")
