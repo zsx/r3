c487c06903d06dea772e28c5710d77966946bc32
Graham Chiu <compkarori@gmail.com>
2017-05-28 10:06:55 +1200

    allow net-log to display server response and body

--- src/mezz/prot-http.r
+++ src/mezz/prot-http.r
@@ -306,12 +306,14 @@ check-response: func [port /local conn res headers d1 d2 line info state awake s
         ]
         remove/part conn/data d2
         state/state: 'reading-data
-        ;if quote (txt) <> last body-of :net-log [
-        ;    print "Dumping Webserver headers and body"
-        ;    net-log/S info
-        ;    body: to string! conn/data
-        ;    dump body
-        ;]
+        if quote (txt) <> last body-of :net-log [ ; net-log is in active state
+            print "Dumping Webserver headers and body"
+            net-log/S info
+            if trap? [
+                body: to string! conn/data
+                dump body
+            ][print ajoin ["S: " length-of conn/data " binary bytes in buffer ..."]]
+        ]
     ]
     unless headers [
         read conn
