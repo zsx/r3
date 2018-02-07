8d201e6758e7d1ae8cb1e68bc5a14a7806343e36
Giulio Lunati <giuliolunati@gmail.com>
2018-02-07 13:18:15 +0100

    Emscripten: add browser build...
    ... and fix Node build.
    
    Build:
    >> cd make
    >> r3 make.r configs/emscripten.r makefile
    >> make clean prep folders r3.html
    
    Then browse to .../make/r3.html
    (maybe you need to serve it on localhost)
    
    Tweak editing make/configs/emscripten.r
    and make/emscripten.html (template for r3.html)

--- make/configs/emscripten.r
+++ make/configs/emscripten.r
@@ -11,16 +11,16 @@ toolset: [
 
 optimize: "z"
 
-main: %../tests/test-node.js
+main: %emscripten.html
 
 debug: true 
 
 ldflags: reduce [
-	unspaced["-O" optimize]
+	unspaced ["-O" optimize]
 	unspaced [{-s 'ASSERTIONS=} either debug [1] [0] {'}]
 	{-s 'EXTRA_EXPORTED_RUNTIME_METHODS=["ccall", "cwrap"]'}
 	{--post-js prep/include/reb-lib.js}
-	either all [main file? main 'file = exists? main] [spaced [{--post-js} main]] [""]
+	spaced [{--shell-file} main]
 ]
 
 extensions: [
