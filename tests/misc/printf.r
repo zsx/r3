2a4d108df1df2963762a4f43a19cae4c9ce7726e
Shixin Zeng <szeng@atronixengineering.com>
2018-03-22 13:22:09 -0400

    Make printf.r run on Windows as well

--- tests/misc/printf.r
+++ tests/misc/printf.r
@@ -3,7 +3,12 @@ REBOL []
 recycle/torture
 
 
-libc: make library! %libc.so.6
+libc: make library! switch system/platform/1 [
+    Linux [%libc.so.6]
+    Windows [%msvcrt.dll]
+] else [
+    fail "don't know where the C library is"
+]
 
 x64?: 40 = fifth system/version
 size_t: either x64? ['int64]['int32]
