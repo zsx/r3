3885339c72bf69ec5cbc7b0a4f3e922cda63e743
Shixin Zeng <szeng@atronixengineering.com>
2019-06-13 13:34:43 -0400

    Update the build files

--- make/msvc/r3/prebuild64.bat
+++ make/msvc/r3/prebuild64.bat
@@ -3,6 +3,11 @@ set T=../../../src/tools
 set OS_ID=0.3.3
 set HOST_PRODUCT_NAME=atronix-view
 
+find /c "%OS_ID%" %T%/../boot/boot-code.r
+if %errorlevel% equ 1 goto notfound
+echo "Already processed"
+goto done
+:notfound
 %REBOL% %T%/make-headers.r
 %REBOL% %T%/make-boot.r %OS_ID% %HOST_PRODUCT_NAME%
 %REBOL% %T%/make-os-ext.r
@@ -10,3 +15,5 @@ set HOST_PRODUCT_NAME=atronix-view
 %REBOL% %T%/core-ext.r
 %REBOL% %T%/view-ext.r
 %REBOL% %T%/saphir-init.r
+
+:done
